71d3d5044666bf0787ba55aab3f8a1e99c6dabe7
Esteban Vergara Giraldo <Teteban0917@gmail.com>
2023-05-31 01:18:53 -0500

    v2.0

--- Form1.py
+++ Form1.py
@@ -21,15 +21,21 @@ class Form1(Form1Template):
 
   #Se ejecuta cuando se presiona el botón [Modo]
   def dia_noche(self, **event_args):
-    modo = anvil.server.call("dia_noche")
     anvil.server.call("titilar")
+    modo = anvil.server.call("dia_noche")
     print(modo)
 
   #Se ejecuta cuando se presiona el botón [Cortina]
   def encendido_apagado(self, **event_args):
-    powermode = anvil.server.call("encendido_apagado")
     anvil.server.call("titilar")
+    powermode = anvil.server.call("encendido_apagado")
     print(powermode)
+
+  #Se ejecuta cuando se presiona el botón [Sensor]
+  def toma_notoma(self, **event_args):
+    anvil.server.call("titilar")
+    toma = anvil.server.call("toma_notoma")
+    print(toma)
      
   #Esta función es llamada cada segundo
   def timer_1_tick(self, **event_args):
@@ -60,6 +66,15 @@ class Form1(Form1Template):
     if len(timestamps) >= 20:
       timestamps.popleft()
 
+    #son para ver información de las variables del raspberry, en los labels
+    self.mode_label.text = anvil.server.call('get_modo')
+    self.powermode_label.text = anvil.server.call('get_powermode')
+    self.toma_label.text = anvil.server.call('get_toma')
+
+    #los valores de la última medición de luz, y el último output de cortina
+    self.luz_label.text = f"{luz} lux" if luz != None else 'Sin datos'
+    self.cortina_label.text = f"{cortina}%"
+
     #podemos graficar
     self.plot_luz.data = go.Scatter(x=list(timestamps) , y=list(luz_values))
     self.plot_luz.layout.title = "Luz ambiental (lux)" 
@@ -67,20 +82,15 @@ class Form1(Form1Template):
     self.plot_cortina.data = go.Scatter( x=list(timestamps) , y=list(cortina_values))
     self.plot_cortina.layout.title = "Apertura de cortina (%)" 
 
-    #son para ver información de las variables del raspberry
-    self.mode_label.text = anvil.server.call('get_modo')
-    self.powermode_label.text = anvil.server.call('get_powermode')
-
-    #los valores de la última medición de luz, y el último output de cortina
-    self.luz_label.text = f"{luz} lux"
-    self.cortina_label.text = f"{cortina}%"
-
-
-
-
-
-
-
-
-
-
+    
+  # se ejecuta automáticamente cada que cambia el contenido del input en el conversor,
+  # o si se presiona enter
+  def conversor(self, **event_args):
+    luz = self.conversor_input.text
+    if luz != None:
+      cortina   = anvil.server.call("conversor",luz)
+      self.conversor_output.text = f"{cortina}%"
+    else:
+      self.conversor_output.text = None
+     
+    
